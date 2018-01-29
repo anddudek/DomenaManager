8155486badcd47fc40422105574bb7739ea3dc0b
Andrzej Dudek <andrzej.dudek@wedzony.com.pl>
2018-01-29 14:05:46 +0100

    EditOwnerWizard

---
+++ DomenaManager/Helpers/ApartmentListView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomenaManager.Helpers
+{
+    public class ApartmentListView
+    {
+        public string BuildingName { get; set; }
+        public int ApartmentNumber { get; set; }
+    }
+}
