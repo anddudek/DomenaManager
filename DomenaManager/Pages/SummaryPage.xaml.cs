eecea973bb6089c0b4312efc4243c6a60762d113
Andrzej Dudek <andrej.dudk@wp.pl>
2018-06-20 22:16:18 +0200

    Fixes in summary page

--- DomenaManager/Pages/SummaryPage.xaml.cs
+++ DomenaManager/Pages/SummaryPage.xaml.cs
@@ -60,43 +60,133 @@ namespace DomenaManager.Pages
                     OnPropertyChanged("SelectedSummary");
                 }
             }
+        }       
+
+        private ObservableCollection<Building> _buildingsNames;
+        public ObservableCollection<Building> BuildingsNames
+        {
+            get { return _buildingsNames; }
+            set
+            {
+                _buildingsNames = value;
+                OnPropertyChanged("BuildingsNames");
+            }
+        }
+
+        private Building _selectedBuildingName;
+        public Building SelectedBuildingName
+        {
+            get { return _selectedBuildingName; }
+            set
+            {
+                _selectedBuildingName = value;
+                InitializeApartmentsNumbers();
+                OnPropertyChanged("ApartmentsNumbers");
+                OnPropertyChanged("SelectedBuildingName");
+            }
+        }
+
+        private ObservableCollection<Apartment> ApartmentsList { get; set; }
+
+        private ObservableCollection<Apartment> _apartmentsNumbers;
+        public ObservableCollection<Apartment> ApartmentsNumbers
+        {
+            get { return _apartmentsNumbers; }
+            set
+            {
+                if (value != _apartmentsNumbers)
+                {
+                    _apartmentsNumbers = value;
+                    OnPropertyChanged("ApartmentsNumbers");
+                }
+            }
+        }
+
+        private ObservableCollection<Owner> OwnersList { get; set; }
+
+        private Apartment _selectedApartmentNumber;
+        public Apartment SelectedApartmentNumber
+        {
+            get { return _selectedApartmentNumber; }
+            set
+            {
+                if (value != _selectedApartmentNumber)
+                {
+                    _selectedApartmentNumber = value;                    
+                    OnPropertyChanged("SelectedApartmentNumber");
+                    if (_selectedApartmentNumber != null)
+                    {
+                        SelectedOwner = OwnersList.FirstOrDefault(x => x.OwnerId.Equals(_selectedApartmentNumber.OwnerId)).OwnerName;
+                    }
+                }
+            }
+        }
+
+        private string _selectedOwner;
+        public string SelectedOwner
+        {
+            get { return _selectedOwner; }
+            set
+            {
+                if (value != _selectedOwner)
+                {
+                    _selectedOwner = value;
+                    OnPropertyChanged("SelectedOwner");
+                }
+            }
+        }
+
+        public ICommand FilterCommand
+        {
+            get { return new RelayCommand(Filter, CanFilter); }
         }
 
         public SummaryPage()
         {
             DataContext = this;
             InitializeComponent();
+            InitializeLists();
+            InitializeApartmentsNumbers();
+            //PrepareData();
+        }
 
+        private void PrepareData(Apartment apartment, int year)
+        {
+            var a = new DataGrid()
+            {
+                AutoGenerateColumns = false,
+                CanUserAddRows = false,
+                CanUserDeleteRows = false,
+                IsReadOnly = true,
+            };
+            var col = new DataGridTextColumn();
+            col.Header = "Miesiąc";
+            col.Binding = new Binding("month");
+            a.Columns.Add(col);
             var sdg = new SummaryDataGrid();
+
             using (var db = new DB.DomenaDBContext())
             {
-                var ap = db.Apartments.FirstOrDefault();
-                var bu = db.Buildings.FirstOrDefault(x => x.BuildingId.Equals(ap.BuildingId));
-                var ow = db.Owners.FirstOrDefault(x => x.OwnerId.Equals(ap.OwnerId));
-                sdg.apartment = ap;
+                var bu = db.Buildings.FirstOrDefault(x => x.BuildingId.Equals(apartment.BuildingId));
+                var ow = db.Owners.FirstOrDefault(x => x.OwnerId.Equals(apartment.OwnerId));
+                sdg.apartment = apartment;
                 sdg.building = bu;
                 sdg.owner = ow;
                 sdg.year = DateTime.Today.Year;
                 sdg.categories = db.CostCategories.ToArray();
                 sdg.rows = new SummaryDataGridRow[12];
-                                
+
                 for (int i = 0; i < 12; i++)
                 {
                     sdg.rows[i] = new SummaryDataGridRow();
-                    sdg.rows[i].month = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(new DateTime(2000, i+1, 1).ToString("MMMM"));
+                    sdg.rows[i].month = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(new DateTime(2000, i + 1, 1).ToString("MMMM"));
                     sdg.rows[i].charges = new string[sdg.categories.Length];
                     for (int j = 0; j < sdg.categories.Length; j++)
                     {
                         sdg.rows[i].charges[j] = (j + i).ToString();
                     }
-                }                
+                }
             }
-            var a = new DataGrid();
-            a.AutoGenerateColumns = false;
-            var col = new DataGridTextColumn();
-            col.Header = "Miesiąc";
-            col.Binding = new Binding("month");
-            a.Columns.Add(col);
             for (int i = 0; i < sdg.categories.Length; i++)
             {
                 var ncol = new DataGridTextColumn();
@@ -106,6 +196,37 @@ namespace DomenaManager.Pages
             }
             a.ItemsSource = sdg.rows;
             SummaryDG = a;
+        }
+
+        private void Filter(object param)
+        {
+            PrepareData(SelectedApartmentNumber, 2018);
+        }
+
+        private bool CanFilter()
+        {
+            return SelectedBuildingName != null && SelectedApartmentNumber != null;
+        }
+
+        private void InitializeApartmentsNumbers()
+        {
+            if (SelectedBuildingName != null)
+            {
+                var a = SelectedBuildingName.BuildingId;
+                var b = ApartmentsList.Where(x => x.BuildingId.Equals(SelectedBuildingName.BuildingId)).ToList();
+                var c = b.Distinct().ToList();
+                ApartmentsNumbers = new ObservableCollection<Apartment>(c.Where(x => x.BuildingId.Equals(SelectedBuildingName.BuildingId)).OrderBy(x => x.ApartmentNumber).ToList());
+            }
+        }
+
+        private void InitializeLists()
+        {
+            using (var db = new DB.DomenaDBContext())
+            {
+                BuildingsNames = new ObservableCollection<Building>(db.Buildings.ToList());
+                ApartmentsList = new ObservableCollection<Apartment>(db.Apartments.ToList());
+                OwnersList = new ObservableCollection<Owner>(db.Owners.ToList());
+            }
         }
 
         private bool IsValid(DependencyObject obj)
