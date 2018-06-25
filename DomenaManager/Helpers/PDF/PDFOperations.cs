b846535fc751debfa76287503340eafb14a3ad5b
Andrzej Dudek <andrej.dudk@wp.pl>
2018-06-25 21:44:19 +0200

    Pdf

--- DomenaManager/Helpers/PDF/PDFOperations.cs
+++ DomenaManager/Helpers/PDF/PDFOperations.cs
@@ -7,6 +7,7 @@ using PdfSharp;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing.Layout;
 
 namespace DomenaManager.Helpers
 {
@@ -18,11 +19,13 @@ namespace DomenaManager.Helpers
             PdfPage page = doc.AddPage();
             XGraphics gfx = XGraphics.FromPdfPage(page);
             XFont standardFont = new XFont("Calibri", 12);
-            gfx.DrawString("Domena", standardFont, XBrushes.Black,
-                new XRect(10, 10, page.Width / 3, page.Height / 5), XStringFormats.TopLeft);
+            XTextFormatter tf = new XTextFormatter(gfx);
+            tf.DrawString("Domena \nManager", standardFont, XBrushes.Black,
+                new XRect(40, 30, page.Width / 3 - 20, page.Height / 5), XStringFormats.TopLeft);
             
             XImage img = XImage.FromGdiPlusImage(DomenaManager.Properties.Resources.Domena);
-            gfx.DrawImage(img, 2 * page.Width / 3, 10, page.Width / 3, page.Height / 5);
+            
+            gfx.DrawImage(img, 2 * page.Width / 3, 30, (page.Width / 3) - 40, page.Width * img.PixelHeight / (3 * img.PixelWidth) - 28);
 
             return doc;
         }
