9d3a448452cbe6a8e80f09beb0bd83c904fcf075
Andrzej Dudek <andrzej.dudek@wedzony.com.pl>
2018-07-11 15:47:02 +0200

    Proceed with summary

--- DomenaManager/Pages/SettlementSummaryPage.xaml.cs
+++ DomenaManager/Pages/SettlementSummaryPage.xaml.cs
@@ -12,20 +12,99 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using DomenaManager.Helpers;
+using MaterialDesignThemes.Wpf;
 
 namespace DomenaManager.Pages
 {
     /// <summary>
     /// Interaction logic for SettlementSummary.xaml
     /// </summary>
-    public partial class SettlementSummaryPage : UserControl
+    public partial class SettlementSummaryPage : UserControl, INotifyPropertyChanged
     {
+        public Visibility IsSettlementPerApartment { get; set; } 
+
+        public Visibility IsSettlementPerMeter { get; set; }
+
+        public Visibility IsSettlementPerArea { get; set; }
+
+        public ICommand SettlementProceed
+        {
+            get { return new RelayCommand(PerformSettlement, CanPerformSettlement); }
+        }
+
+        public ICommand SettlementBack
+        {
+            get { return new RelayCommand(CancelSettlement, CanPerformSettlement); }
+        }
         private SettlementPage _settlementPage;
 
         public SettlementSummaryPage(SettlementPage settlementPage)
         {
             _settlementPage = settlementPage;
+            IsSettlementPerApartment = Visibility.Collapsed;
+            IsSettlementPerMeter = Visibility.Collapsed;
+            IsSettlementPerArea = Visibility.Collapsed;
+            switch (settlementPage.SettlementMethod)
+            {
+                default:
+                    break;
+                case SettlementMethodsEnum.PER_APARTMENT:
+                    IsSettlementPerApartment = System.Windows.Visibility.Visible;
+                    break;
+                case SettlementMethodsEnum.PER_AREA:
+                    IsSettlementPerArea = System.Windows.Visibility.Visible;
+                    break;
+                case SettlementMethodsEnum.PER_METERS:
+                    IsSettlementPerMeter = System.Windows.Visibility.Visible;
+                    break;
+            }
+            DataContext = this;
+            PopulateData();
             InitializeComponent();
         }
-    }
+
+        private void PopulateData()
+        {
+
+        }
+
+        private void PerformSettlement(object param)
+        {
+
+        }
+
+        private void CancelSettlement(object param)
+        {
+            var mw = (((((this.Parent as MahApps.Metro.Controls.TransitioningContentControl).Parent as Grid).Parent as DialogHost).Parent as DialogHost).Parent as DialogHost).Parent as Windows.MainWindow;
+
+            mw.CurrentPage = _settlementPage;
+        }
+
+        private bool CanPerformSettlement()
+        {
+            return true;
+        }
+
+        private bool IsValid(DependencyObject obj)
+        {
+            // The dependency object is valid if it has no errors and all
+            // of its children (that are dependency objects) are error-free.
+            return !Validation.GetHasError(obj) &&
+            LogicalTreeHelper.GetChildren(obj)
+            .OfType<DependencyObject>()
+            .All(IsValid);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }   
 }
